9da986f7dcd30df1b4ad82e8b017ec460378b70a
Shawn Conerton <conerton52@gmail.com>
2021-02-16 14:09:44 -0600

    breaks when button is pressed to add a new game. Moving on to chapter 10 until I get hetlp to fix.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { GameList } from "./game/GameList.js";
 import { GameProvider } from "./game/GameProvider.js";
-import { EventProvider } from "./game/EventProvider";
-import { EventList } from "./game/EventList";
+import { EventProvider } from "./event/EventProvider";
+import { EventList } from "./event/EventList";
+import { GameForm } from "./game/GameForm";
 
-export const ApplicationViews = () => {
+export const ApplicationViews = (props) => {
   return (
     <>
       <main
@@ -15,10 +16,19 @@ export const ApplicationViews = () => {
         }}
       >
         <GameProvider>
-          <Route exact path="/">
+          <Route exact path="/games">
             <GameList />
           </Route>
         </GameProvider>
+
+        <GameProvider>
+          <EventProvider>
+            <Route exact path="/games/new">
+              <GameForm {...props} />
+            </Route>
+          </EventProvider>
+        </GameProvider>
+
         <EventProvider>
           <Route exact path="/events">
             <EventList />
